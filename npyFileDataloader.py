f250d275a79ce9b0d042a567ac5dd284d3265b24
hmze-git <hamzahrassul92@gmail.com>
2026-09-04 20:29:35 +0200

    Early stopping agumentation etc

--- npyFileDataloader.py
+++ npyFileDataloader.py
@@ -1,6 +1,7 @@
 import torch
 import numpy as np
 from torch.utils.data import Dataset
+from torchvision.transforms import v2
 
 class NumpyLoader(Dataset):
     def __init__(self,xPath,yPath):
@@ -15,6 +16,15 @@ class NumpyLoader(Dataset):
         #Permute so channel comes before the other 2 otherwise it fails with the ViT 
         #maybe this will break the batching or vidoes idk check later 
         x=torch.from_numpy(np.asarray(self.xData[idx])).permute(0,3,1,2).contiguous()
+
+        
+        transformed=v2.Compose([
+            v2.RandomResizedCrop(size=(224,224),antialias=True),
+            v2.ColorJitter(brightness=0.3,contrast=0.2,saturation=0.2),
+            v2.RandomHorizontalFlip(0.5)
+        ])
+
+        x=transformed(x)
         y=torch.from_numpy(np.asarray(self.yData[idx]))
 
         return x,y
--- run.py
+++ run.py
@@ -9,16 +9,27 @@ import torchinfo
 import torchmetrics
 from torchmetrics import classification
 from ViT import SkinCancerLSTMViT
-
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+import matplotlib as plt
 device=None
 if torch.cuda.is_available:
   device=torch.device('cuda')
 
+def setSeed():
+  seed=67
+
+  torch.manual_seed(seed)
+  torch.cuda.manual_seed_all(seed)
+
+
+
+setSeed()
 
 NpFile=npyFileDataloader.NumpyLoader(r"C:\Users\Hamzah\Desktop\HYP\Honours Project\Pipeline1TFlow\InputFiltered.npy",r"C:\Users\Hamzah\Desktop\HYP\Honours Project\Pipeline1TFlow\OutputTags.npy")
 NPFileTest=npyFileDataloader.NumpyLoader(r"C:\Users\Hamzah\Desktop\HYP\Honours Project\Pipeline1TFlow\validationFiltered.npy",r"C:\Users\Hamzah\Desktop\HYP\Honours Project\Pipeline1TFlow\validationTags.npy")
-#smallTest=Subset(NpFile,list(range(12)))
-#smallLoader=DataLoader(smallTest,batch_size=4,shuffle=True)
+
+smallTest=Subset(NpFile,list(range(12)))
+smallLoader=DataLoader(smallTest,batch_size=1,shuffle=True)
 
 trainLoader=DataLoader(NpFile,batch_size=4,shuffle=True)
 testLoader=DataLoader(NPFileTest,batch_size=4) # dont shuffle so that when testing it gets items in same order so it wont fluctuate based on what was given first
@@ -37,13 +48,20 @@ preTrainedViT.eval()
 
 FullModel=SkinCancerLSTMViT(preTrainedViT,hiddenSize,inputDim,True,3)
 
-learningRate=0.0005
+learningRate=0.001
 
 
 
 lossFunction=nn.CrossEntropyLoss()
 validationLossFunction=nn.CrossEntropyLoss()
 adamOptimiser=torch.optim.Adam(params=FullModel.parameters(),lr=learningRate)
+
+#tr step lr reduce learn rate by 10X every 10 epochs
+#if this does not work try ReduceLRONPateu for when valid accuracy taps out
+
+stepLearnDecay=torch.optim.lr_scheduler.StepLR(adamOptimiser,5,0.1)
+stepLearnDecay.optimizer.zero_grad
+
 metric=classification.Accuracy(task='multiclass',num_classes=3)
 testMetric=classification.Accuracy(task='multiclass',num_classes=3)
 metric=metric.to(device)
@@ -54,7 +72,8 @@ if device is not None:
 
 
 
-def trainStep(model,dataLoader,testLoader,metric,testMetric,lossFunction,testLossFunction,optimiser,epoch):
+
+def trainStep(model,dataLoader,testLoader,metric,testMetric,lossFunction,testLossFunction,optimiser,epoch,lossArr,valLossArr,accArr,valAccArr):
   #put mode; in train mode
   #change to model.eval whne doing validation loss etc
   model.train()
@@ -101,16 +120,99 @@ def trainStep(model,dataLoader,testLoader,metric,testMetric,lossFunction,testLos
   validNormLoss=validationLoss/len(testLoader)
 
   print(f"Epoch {epoch} | Loss {normLoss} | Validation Loss {validNormLoss} | Accuracy {epochAccuracy} | Valid Accuracy {epochValidationAccuary}")
+  lossArr.append(normLoss)
+  valLossArr.append(validNormLoss)
+  accArr.append(epochAccuracy)
+  valAccArr.append(epochValidationAccuary)
 
     
 
    
-def trainingLoop(epochs,model,dataLoad,testDataLoad,lossFN,testLossFn,Optimiser,metric,testMetric):
- 
+def trainingLoop(epochs,model,dataLoad,testDataLoad,lossFN,testLossFn,Optimiser,lrDecay,metric,testMetric):
+
+  lossArr=[]
+  valLossArr=[]
+  accArr=[]
+  valAcc=[]
+
+  bestValAcc=0.0
   for e in range(epochs):
-      trainStep(model,dataLoad,testDataLoad,metric,testMetric,lossFN,testLossFn,Optimiser,e)
+      
+      trainStep(model,dataLoad,testDataLoad,metric,testMetric,lossFN,testLossFn,Optimiser,e,lossArr,valLossArr,accArr,valAcc)
+
+      #get the very last accuracy and see if higher than best
+      mrValAcc=valAcc[-1]
+
+
+      #Basic checkpoint hist
+      if mrValAcc>bestValAcc:
+         bestValAcc=mrValAcc
+         saveModel(model,e,Optimiser,valLossArr[-1],lossArr[-1])
+
+
+
       metric.reset()
       testMetric.reset()
+      lrDecay.step()
+
+  savedModelDict=torch.load('savedModel.tar')
+
+  model.load_state_dict(savedModelDict['modelStateDict'])  
+  model.eval()
+  with torch.inference_mode():
+
+    yPredict=[] 
+    yTrue=[]
+    for batch,(x,y) in enumerate(testLoader):
+        validInput=x.to(device)
+        validLabel=y.to(device)
+
+        validationPreds=model(validInput)
+        validIndex=torch.argmax(validationPreds)
+        yPredict.append(validIndex)
+        yTrue.append(y)
+
+    confMatrix=confusion_matrix(yTrue,yPredict)
+    disp=ConfusionMatrixDisplay(confusion_matrix=confMatrix,display_labels=[0,1,2])
+    disp.plot(cmap='Blues')
+    plt.savefig('confMatrix.png')
+    plt.show()
+    plt.clf()
+    #accuracy and loss plotts
+    plt.plot(accArr)
+    plt.plot(valAcc)
+    plt.title('Model Accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train','val'])
+    plt.savefig('NormVsValACc.png')
+    plt.show()
+    plt.clf()
+
+    plt.plot(lossArr)
+    plt.plot(valLossArr)
+    plt.title('Model Loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train','val'])
+    plt.show()
+    plt.savefig('NormVsValLoss.png')
 
 
-trainingLoop(5,FullModel,trainLoader,testLoader,lossFunction,validationLossFunction,adamOptimiser,metric,testMetric)
+
+    
+def saveModel(model,epoch,optimiser,vLoss,loss):
+
+   torch.save({
+      'epoch':epoch,
+      'modelStateDict':model.state_dict(),
+      'optimiserStateDict':optimiser.state_dict(),
+      'valLoss':vLoss,
+      'loss':loss
+   },'savedModel.tar')
+   
+
+  
+
+
+trainingLoop(5,FullModel,trainLoader,testLoader,lossFunction,validationLossFunction,adamOptimiser,stepLearnDecay,metric,testMetric)
